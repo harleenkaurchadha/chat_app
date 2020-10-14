cdc79032752f7bab941846744ee8b260bf1efdaa
anmol <anmol@gagandeepsinghsuri.local>
2020-10-15 01:34:07 +0530

    creating chat bubbles and chat layout

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,13 +1,23 @@
+import './message_bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 
 class Messages extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-   return StreamBuilder(
-     stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
-     builder: (ctx,chatSnapshot) {
+   return FutureBuilder(
+     future: FirebaseAuth.instance.currentUser(),
+     builder: (ctx, futureSnapshot) {
+       if(futureSnapshot.connectionState == ConnectionState.waiting) {
+         return Center(
+           child: CircularProgressIndicator(),
+         );
+       }
+        return StreamBuilder(
+          stream: Firestore.instance.collection('chat').orderBy('createdAt', descending: true).snapshots(),
+          builder: (ctx,chatSnapshot) {
        if(chatSnapshot.connectionState == ConnectionState.waiting) {
          return Center(
            child: CircularProgressIndicator(),
@@ -15,13 +25,17 @@ class Messages extends StatelessWidget{
        }
        final chatDocs = chatSnapshot.data.documents;
        return ListView.builder(
-         reverse: true,                                                     //to order new message at bottom
-         itemCount: chatDocs.length,
-         itemBuilder: (ctx, index) => Text(
-             chatDocs[index]['text'],
-         ),
+             reverse: true, //to order new message at bottom
+             itemCount: chatDocs.length,
+             itemBuilder: (ctx, index) => MessageBubble(
+                   chatDocs[index]['text'],
+                   chatDocs[index]['userId'] == futureSnapshot.data.uid,
+                   key: ValueKey(chatDocs[index].documentID),               //every message has a unique id which is document ID
+                 ),
+           );
+         },
        );
-     } ,
+     },
    );
   }
 }
